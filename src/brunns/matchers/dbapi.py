74701e45bacc7dd7634f35dd9903c418f8b55cd2
Simon Brunning <simon@brunningonline.net>
2018-11-26 15:19:43 +0000

    Add database has_table_with_rows matcher.

--- src/brunns/matchers/dbapi.py
+++ src/brunns/matchers/dbapi.py
@@ -5,19 +5,25 @@ import logging
 
 from hamcrest.core.base_matcher import BaseMatcher
 
+from brunns.utils import dtuple
+
 logger = logging.getLogger(__name__)
 
 
-def has_table(named):
-    return HasTable(named)
+def has_table(table):
+    return HasTable(table)
+
+
+def has_table_with_rows(table, row_matcher):
+    return HasTableWithRows(table, row_matcher)
 
 
 class HasTable(BaseMatcher):
-    def __init__(self, named):
-        self.named = named
+    def __init__(self, table):
+        self.table = table
 
     def _matches(self, conn):
-        select = "SELECT * FROM {0};".format(self.named)  # nosec
+        select = "SELECT * FROM {0};".format(self.table)  # nosec
         try:
             cursor = conn.cursor()
             return cursor.execute(select)
@@ -25,13 +31,54 @@ class HasTable(BaseMatcher):
             return False
 
     def describe_to(self, description):
-        description.append_text("DB connection has table named ").append_description_of(self.named)
+        description.append_text("DB connection has table named ").append_description_of(self.table)
 
     def describe_mismatch(self, conn, mismatch_description):
-        select = "SELECT * FROM {0};".format(self.named)  # nosec
+        select = "SELECT * FROM {0};".format(self.table)  # nosec
         try:
             cursor = conn.cursor()
             cursor.execute(select)
+        except Exception as e:
+            return (
+                mismatch_description.append_text("SQL statement ")
+                .append_description_of(select)
+                .append_text(" gives ")
+                .append_description_of(type(e).__name__)
+                .append_text(" ")
+                .append_description_of(e)
+            )
+
+
+class HasTableWithRows(BaseMatcher):
+    def __init__(self, table, row_matcher):
+        self.table = table
+        self.row_matcher = row_matcher
+
+    def _matches(self, conn):
+        select = "SELECT * FROM {0};".format(self.table)  # nosec
+        try:
+            rows = self._get_rows(conn, select)
+            return self.row_matcher.matches(rows)
+        except Exception:
+            return False
+
+    def _get_rows(self, conn, select):
+        cursor = conn.cursor()
+        cursor.execute(select)
+        descriptor = dtuple.TupleDescriptor(cursor.description)
+        rows = [dtuple.DatabaseTuple(descriptor, row) for row in cursor.fetchall()]
+        return rows
+
+    def describe_to(self, description):
+        description.append_text("DB connection with table ").append_description_of(self.table).append_text(
+            " with rows matching "
+        ).append_description_of(self.row_matcher)
+
+    def describe_mismatch(self, conn, mismatch_description):
+        select = "SELECT * FROM {0};".format(self.table)  # nosec
+        try:
+            rows = self._get_rows(conn, select)
+            self.row_matcher.describe_mismatch(rows, mismatch_description)
         except Exception as e:
             return (
                 mismatch_description.append_text("SQL statement ")
--- tests/unit/matchers/test_dbapi.py
+++ tests/unit/matchers/test_dbapi.py
@@ -4,9 +4,9 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import logging
 import sqlite3
 
-from hamcrest import assert_that, has_string, not_
+from hamcrest import assert_that, has_string, not_, contains, has_properties
 
-from brunns.matchers.dbapi import has_table
+from brunns.matchers.dbapi import has_table, has_table_with_rows
 from brunns.matchers.matcher import mismatches_with
 
 import pytest
@@ -34,5 +34,39 @@ def test_has_table(db):
     assert_that(has_table("sausages"), has_string("DB connection has table named 'sausages'"))
     assert_that(
         has_table("bacon"),
+        mismatches_with(db, "SQL statement 'SELECT * FROM bacon;' gives 'OperationalError' <no such table: bacon>"),
+    )
+
+
+def test_has_rows(db):
+    assert_that(
+        db,
+        has_table_with_rows(
+            "sausages", contains(has_properties(kind="cumberland"), has_properties(kind="lincolnshire"))
+        ),
+    )
+    assert_that(db, not_(has_table_with_rows("sausages", contains(has_properties(kind="vegetarian")))))
+    assert_that(
+        db, not_(has_table_with_rows("bacon", contains(has_properties(kind="smoked"), has_properties(kind="unsmoked"))))
+    )
+    assert_that(
+        has_table_with_rows(
+            "sausages", contains(has_properties(kind="cumberland"), has_properties(kind="lincolnshire"))
+        ),
+        has_string(
+            "DB connection with table 'sausages' with rows matching a sequence containing ["
+            "(an object with a property 'kind' matching 'cumberland'), "
+            "(an object with a property 'kind' matching 'lincolnshire')"
+            "]"
+        ),
+    )
+    assert_that(
+        has_table_with_rows("sausages", contains(has_properties(kind="vegetarian"))),
+        mismatches_with(
+            db, "item 0: an object with a property 'kind' matching 'vegetarian' property 'kind' was 'cumberland'"
+        ),
+    )
+    assert_that(
+        has_table_with_rows("bacon", contains(has_properties(kind="smoked"), has_properties(kind="unsmoked"))),
         mismatches_with(db, "SQL statement 'SELECT * FROM bacon;' gives 'OperationalError' <no such table: bacon>"),
     )

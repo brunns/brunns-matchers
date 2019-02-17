bcdfb7aabfd24fcc173d29cc28845c11769d0295
Simon Brunning <simon@brunningonline.net>
2019-02-17 08:35:49 +0000

    Matchers for URL queries & fragments.

--- src/brunns/matchers/url.py
+++ src/brunns/matchers/url.py
@@ -18,19 +18,29 @@ def with_path(matcher):
     return UrlWith(path=matcher)
 
 
+def with_query(matcher):
+    return UrlWith(query=matcher)
+
+
+def with_fragment(matcher):
+    return UrlWith(fragment=matcher)
+
+
 class UrlWith(BaseMatcher):
-    def __init__(self, host=ANYTHING, path=ANYTHING, query=ANYTHING):
+    def __init__(self, host=ANYTHING, path=ANYTHING, query=ANYTHING, fragment=ANYTHING):
         super(UrlWith, self).__init__()
         self.host = wrap_matcher(host)
         self.path = wrap_matcher(path)
         self.query = wrap_matcher(query)
+        self.fragment = wrap_matcher(fragment)
 
     def _matches(self, url):
         url = url if isinstance(url, furl.furl) else furl.furl(url)
         return (
             self.host.matches(url.host)
             and self.path.matches(url.path)
-            and self.query.matches(url.query)
+            and self.query.matches(url.query.params)
+            and self.fragment.matches(url.fragment)
         )
 
     def describe_to(self, description):
@@ -39,6 +49,10 @@ class UrlWith(BaseMatcher):
             description.append_text(" host ").append_description_of(self.host)
         if self.path != ANYTHING:
             description.append_text(" path ").append_description_of(self.path)
+        if self.query != ANYTHING:
+            description.append_text(" query ").append_description_of(self.query)
+        if self.fragment != ANYTHING:
+            description.append_text(" fragment ").append_description_of(self.fragment)
 
     def describe_mismatch(self, url, mismatch_description):
         url = url if isinstance(url, furl.furl) else furl.furl(url)
@@ -48,3 +62,9 @@ class UrlWith(BaseMatcher):
         if self.path != ANYTHING and not self.path.matches(url.path):
             mismatch_description.append_text("path ")
             self.host.describe_mismatch(url.path, mismatch_description)
+        if self.query != ANYTHING and not self.query.matches(url.query.params):
+            mismatch_description.append_text("query ")
+            self.query.describe_mismatch(url.query.params, mismatch_description)
+        if self.fragment != ANYTHING and not self.fragment.matches(url.fragment):
+            mismatch_description.append_text("fragment ")
+            self.host.describe_mismatch(url.fragment, mismatch_description)
--- tests/unit/matchers/test_url.py
+++ tests/unit/matchers/test_url.py
@@ -1,16 +1,17 @@
 # encoding=utf-8
 import logging
 
-from hamcrest import assert_that, not_, has_string
+from hamcrest import assert_that, not_, has_string, has_entries
 
 from brunns.matchers.matcher import mismatches_with
-from brunns.matchers.url import to_host, with_path
+from brunns.matchers.url import to_host, with_path, with_query, with_fragment
 
 logger = logging.getLogger(__name__)
 
+URL = "http://brunni.ng/path?key1=value1&key2=value2#fragment"
+
 
 def test_to_host():
-    URL = "http://brunni.ng/path"
     should_match = to_host("brunni.ng")
     should_not_match = to_host("example.com")
 
@@ -22,7 +23,6 @@ def test_to_host():
 
 
 def test_with_path():
-    URL = "http://brunni.ng/path"
     should_match = with_path("/path")
     should_not_match = with_path("/banana")
 
@@ -31,3 +31,28 @@ def test_with_path():
 
     assert_that(should_match, has_string("URL with path '/path'"))
     assert_that(should_not_match, mismatches_with(URL, "path was </path>"))
+
+
+def test_with_query():
+    should_match = with_query(has_entries(key1="value1", key2="value2"))
+    should_not_match = with_query(has_entries(key1="value1", key2="nope"))
+
+    assert_that(URL, should_match)
+    assert_that(URL, not_(should_not_match))
+
+    assert_that(
+        should_match,
+        has_string("URL with query a dictionary containing {'key1': 'value1', 'key2': 'value2'}"),
+    )
+    assert_that(should_not_match, mismatches_with(URL, "query value for 'key2' was 'value2'"))
+
+
+def test_with_fragment():
+    should_match = with_fragment("fragment")
+    should_not_match = with_fragment("banana")
+
+    assert_that(URL, should_match)
+    assert_that(URL, not_(should_not_match))
+
+    assert_that(should_match, has_string("URL with fragment 'fragment'"))
+    assert_that(should_not_match, mismatches_with(URL, "fragment was <fragment>"))

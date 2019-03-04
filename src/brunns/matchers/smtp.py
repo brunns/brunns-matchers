a9037e8358255114ca6339f9a8f53042d83e9458
Simon Brunning <simon@brunningonline.net>
2019-03-04 09:37:20 +0000

    Add from name and address to email matcher.

--- src/brunns/matchers/smtp.py
+++ src/brunns/matchers/smtp.py
@@ -9,12 +9,23 @@ from hamcrest.core.helpers.wrap_matcher import wrap_matcher
 ANYTHING = anything()
 
 
-def email_with(to_name=ANYTHING, to_address=ANYTHING, subject=ANYTHING, body_text=ANYTHING):
+def email_with(
+    to_name=ANYTHING,
+    to_address=ANYTHING,
+    from_name=ANYTHING,
+    from_address=ANYTHING,
+    subject=ANYTHING,
+    body_text=ANYTHING,
+):
     """Match email with
     :param to_name:
     :type to_name: Matcher(str) or str
     :param to_address:
     :type to_address: Matcher(str) or str
+    :param from_name:
+    :type from_name: Matcher(str) or str
+    :param from_address:
+    :type from_address: Matcher(str) or str
     :param subject:
     :type subject: Matcher(str) or str
     :param body_text:
@@ -22,23 +33,42 @@ def email_with(to_name=ANYTHING, to_address=ANYTHING, subject=ANYTHING, body_tex
     :return: Matcher
     :rtype: Matcher(str)
     """
-    return EmailWith(to_name=to_name, to_address=to_address, subject=subject, body_text=body_text)
+    return EmailWith(
+        to_name=to_name,
+        to_address=to_address,
+        from_name=from_name,
+        from_address=from_address,
+        subject=subject,
+        body_text=body_text,
+    )
 
 
 class EmailWith(BaseMatcher):
-    def __init__(self, to_name=ANYTHING, to_address=ANYTHING, subject=ANYTHING, body_text=ANYTHING):
+    def __init__(
+        self,
+        to_name=ANYTHING,
+        to_address=ANYTHING,
+        from_name=ANYTHING,
+        from_address=ANYTHING,
+        subject=ANYTHING,
+        body_text=ANYTHING,
+    ):
         self.to_name = wrap_matcher(to_name)
         self.to_address = wrap_matcher(to_address)
+        self.from_name = wrap_matcher(from_name)
+        self.from_address = wrap_matcher(from_address)
         self.subject = wrap_matcher(subject)
         self.body_text = wrap_matcher(body_text)
 
     def _matches(self, actual_email):
-        actual_to_name, actual_to_address, actual_subject, actual_body_text = self._parse_email(
+        actual_to_name, actual_to_address, actual_from_name, actual_from_address, actual_subject, actual_body_text = self._parse_email(
             actual_email
         )
         return (
             self.to_name.matches(actual_to_name)
             and self.to_address.matches(actual_to_address)
+            and self.from_name.matches(actual_from_name)
+            and self.from_address.matches(actual_from_address)
             and self.subject.matches(actual_subject)
             and self.body_text.matches(actual_body_text)
         )
@@ -47,24 +77,45 @@ class EmailWith(BaseMatcher):
     def _parse_email(actual_email):
         parsed = email.message_from_string(actual_email)
         actual_to_name, actual_to_address = re.match("(.*) <(.*)>", parsed["To"]).groups()
+        actual_from_name, actual_from_address = re.match("(.*) <(.*)>", parsed["From"]).groups()
         actual_subject = parsed["Subject"]
         actual_body_text = parsed.get_payload()
-        return actual_to_name, actual_to_address, actual_subject, actual_body_text
+        return (
+            actual_to_name,
+            actual_to_address,
+            actual_from_name,
+            actual_from_address,
+            actual_subject,
+            actual_body_text,
+        )
 
     def describe_to(self, description):
         description.append_text(
-            "email with to_name {0} to_address {1} subject {2} body_text {3}".format(
-                self.to_name, self.to_address, self.subject, self.body_text
+            "email with to_name {0} to_address {1} from_name {2} from_address {3} subject {4} body_text {5}".format(
+                self.to_name,
+                self.to_address,
+                self.from_name,
+                self.from_address,
+                self.subject,
+                self.body_text,
             )
         )
 
     def describe_mismatch(self, actual_email, mismatch_description):
-        actual_to_name, actual_to_address, actual_subject, actual_body_text = self._parse_email(
+        actual_to_name, actual_to_address, actual_from_name, actual_from_address, actual_subject, actual_body_text = self._parse_email(
             actual_email
         )
         mismatch_description.append_text("was to_name ").append_description_of(
             actual_to_name
         ).append_text(" to_address ").append_description_of(actual_to_address).append_text(
+            " from_name "
+        ).append_description_of(
+            actual_from_name
+        ).append_text(
+            " from_address "
+        ).append_description_of(
+            actual_from_address
+        ).append_text(
             " subject "
         ).append_description_of(
             actual_subject
--- tests/unit/matchers/test_smtp.py
+++ tests/unit/matchers/test_smtp.py
@@ -8,10 +8,12 @@ from tests.utils.builders import email_message_builder
 
 def test_email_matcher():
     # Given
-    m = (
+    message = (
         email_message_builder()
-        .with_to_name("fred")
+        .with_to_name("simon")
         .with_to_email_address("simon@brunni.ng")
+        .with_from_name("fred")
+        .with_from_email_address("fred@beardy.dev")
         .with_subject("chips")
         .with_body_text("bananas")
         .as_string()
@@ -20,17 +22,20 @@ def test_email_matcher():
     # When
 
     # Then
-    assert_that(m, email_with(to_name="fred"))
-    assert_that(m, not_(email_with(to_name="Banana")))
+    assert_that(message, email_with(to_name="simon"))
+    assert_that(message, not_(email_with(to_name="Banana")))
     assert_that(
         email_with(body_text="Foobar"),
         has_string(
-            "email with to_name ANYTHING to_address ANYTHING subject ANYTHING body_text 'Foobar'"
+            "email with to_name ANYTHING to_address ANYTHING from_name ANYTHING from_address ANYTHING "
+            "subject ANYTHING body_text 'Foobar'"
         ),
     )
     assert_that(
         email_with(to_name="Banana"),
         mismatches_with(
-            m, "was to_name 'fred' to_address 'simon@brunni.ng' subject 'chips' body_text 'bananas'"
+            message,
+            "was to_name 'simon' to_address 'simon@brunni.ng' from_name 'fred' from_address 'fred@beardy.dev' "
+            "subject 'chips' body_text 'bananas'",
         ),
     )

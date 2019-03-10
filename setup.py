e88e2fab66118b69ccf4b8863f8823ee0c730a90
Simon Brunning <simon@brunningonline.net>
2019-03-10 20:02:25 +0000

    Release 2.1.0

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ except IOError:
 setup(
     name="brunns-matchers",
     zip_safe=False,
-    version="2.0.0",
+    version="2.1.0",
     description="Custom PyHamcrest matchers",
     long_description=long_description,
     long_description_content_type="text/markdown",

010e118a8f2bab63bbdbab3df9f29a210b787f69
Simon Brunning <simon@brunningonline.net>
2020-03-10 13:13:36 +0000

    Add bytestring matcher.

---
+++ src/brunns/matchers/bytestring.py
@@ -0,0 +1,26 @@
+# encoding=utf-8
+import logging
+
+from hamcrest.core.base_matcher import BaseMatcher
+from hamcrest.core.description import Description
+
+logger = logging.getLogger(__name__)
+
+
+class ContainsBytestring(BaseMatcher[bytes]):
+    """TODO"""
+
+    def __init__(self, bytestring: bytes) -> None:
+        super().__init__()
+        self.bytestring = bytestring
+
+    def _matches(self, item: bytes) -> bool:
+        return self.bytestring in item
+
+    def describe_to(self, description: Description) -> None:
+        description.append_text("bytestring containing ").append_description_of(self.bytestring)
+
+
+def contains_bytestring(bytestring: bytes) -> ContainsBytestring:
+    """TODO"""
+    return ContainsBytestring(bytestring)
---
+++ tests/unit/matchers/test_bytestring.py
@@ -0,0 +1,19 @@
+# encoding=utf-8
+import logging
+
+from brunns.matchers.bytestring import contains_bytestring
+from brunns.matchers.matcher import mismatches_with
+from hamcrest import assert_that, has_string, not_
+
+logger = logging.getLogger(__name__)
+
+
+def test_contains_bytestring():
+    should_match = contains_bytestring(b"foo")
+    should_not_match = contains_bytestring(b"bar")
+
+    assert_that(b"a foo b", should_match)
+    assert_that(b"a foo b", not_(should_not_match))
+
+    assert_that(should_match, has_string("bytestring containing <b'foo'>"))
+    assert_that(should_not_match, mismatches_with(b" a foo b", "was <b' a foo b'>"))

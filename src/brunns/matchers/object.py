cc6cf766ed131950b26ae9f3eca10a4cbbcad36d
Simon Brunning <simon@brunningonline.net>
2020-05-12 10:13:07 +0100

    Add ignoring argument to has_identical_properties_to() matcher.

--- src/brunns/matchers/object.py
+++ src/brunns/matchers/object.py
@@ -2,7 +2,7 @@
 import collections
 import inspect
 from itertools import zip_longest
-from typing import Any, Mapping, Union
+from typing import Any, Iterable, Mapping, Optional, Union
 
 from hamcrest import (
     all_of,
@@ -39,35 +39,43 @@ class HasRepr(BaseMatcher[Any]):
         self.expected.describe_to(description)
 
 
-def has_identical_properties_to(expected: Any) -> Matcher[Any]:
+def has_identical_properties_to(
+    expected: Any, ignoring: Optional[Iterable[str]] = None
+) -> Matcher[Any]:
     """Matches object with identical properties to
     :param expected: Expected object
-    :return: Matcher(object)
+    :param ignoring: Collection of names to ignore in comparisons
     """
-    return HasIdenticalPropertiesTo(expected)
+    return HasIdenticalPropertiesTo(expected, ignoring=ignoring)
 
 
 class HasIdenticalPropertiesTo(BaseMatcher[Any]):
-    def __init__(self, expected: Any) -> None:
+    def __init__(self, expected: Any, ignoring: Optional[Iterable[str]] = None) -> None:
         self.expected = expected
+        self.ignoring = ignoring
 
     def _matches(self, actual: Any) -> bool:
-        return equal_vars(actual, self.expected)
+        return equal_vars(actual, self.expected, ignoring=self.ignoring)
 
     def describe_to(self, description: Description) -> None:
         description.append_text(
             "object with identical properties to object "
         ).append_description_of(self.expected)
+        if self.ignoring:
+            description.append_text(" ignoring properties named ").append_list(
+                "{", ", ", "}", self.ignoring
+            )
 
 
-def equal_vars(left: Any, right: Any) -> bool:
+def equal_vars(left: Any, right: Any, ignoring: Optional[Iterable[str]] = None) -> bool:
     """Test if two objects are equal using public vars() and properties if available, with == otherwise."""
     try:
-        left_vars = _vars_and_properties(left)
-        right_vars = _vars_and_properties(right)
+        left_vars = _vars_and_properties(left, ignoring=ignoring)
+        right_vars = _vars_and_properties(right, ignoring=ignoring)
         if left_vars:
             return left_vars.keys() == right_vars.keys() and all(
-                equal_vars(right_vars[key], value) for key, value in left_vars.items()
+                equal_vars(right_vars[key], value, ignoring=ignoring)
+                for key, value in left_vars.items()
             )
     except TypeError:
         pass
@@ -90,14 +98,21 @@ def _equal_vars_for_non_objects(left: Any, right: Any) -> bool:
         return left == right
 
 
-def _vars_and_properties(obj: Any) -> Mapping[str, Any]:
-    """Get an object's public vars() and properties. Raises TypeError if not an obcect with vars()/"""
-    vars_and_props = {key: value for key, value in vars(obj).items() if not key.startswith("_")}
+def _vars_and_properties(obj: Any, ignoring: Optional[Iterable[str]] = None) -> Mapping[str, Any]:
+    """Get an object's public vars() and properties. Raises TypeError if not an object with vars()."""
+    ignoring = ignoring or {}
+    vars_and_props = {
+        key: value
+        for key, value in vars(obj).items()
+        if not key.startswith("_") and key not in ignoring
+    }  # vars
     classes = inspect.getmembers(obj, inspect.isclass)
-    for cls in classes:
+    for cls in classes:  # props
         props = inspect.getmembers(cls[1], lambda o: isinstance(o, property))
         for prop in props:
-            vars_and_props[prop[0]] = getattr(obj, prop[0])
+            name = prop[0]
+            if name not in ignoring:
+                vars_and_props[name] = getattr(obj, name)
     return vars_and_props
 
 

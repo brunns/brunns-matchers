a9bef1d979380860489070ffe8f2417a0b942a32
Simon Brunning <simon@brunningonline.net>
2021-03-04 12:24:51 +0000

    Add `matches_with` matcher, so I can start writing some `describe_matches()` overrides where they'll be useful.

--- src/brunns/matchers/matcher.py
+++ src/brunns/matchers/matcher.py
@@ -2,7 +2,7 @@
 import difflib
 from typing import Any, Union
 
-from hamcrest import anything, not_
+from hamcrest import anything
 from hamcrest.core.base_matcher import BaseMatcher
 from hamcrest.core.core.isequal import IsEqual
 from hamcrest.core.description import Description
@@ -33,6 +33,9 @@ class MismatchesWith(BaseMatcher[Matcher]):
             description.append_text("matched")
             return
         description.append_text("got message ").append_description_of(actual_message)
+        self.append_diff(actual_message, description)
+
+    def append_diff(self, actual_message, description):
         if isinstance(self.expected_message, IsEqual) and isinstance(
             self.expected_message.object, str
         ):
@@ -53,6 +56,47 @@ def mismatches(value_not_to_match: Any) -> MismatchesWith:
     return MismatchesWith(value_not_to_match, anything())
 
 
-def matches(value_to_match: Any) -> Matcher:
+class MatchesWith(BaseMatcher[Matcher]):
+    def __init__(self, value_to_match: Any, expected_message: Union[str, Matcher[str]]) -> None:
+        super(MatchesWith, self).__init__()
+        self.value_to_match = value_to_match
+        self.expected_message: Matcher[str] = wrap_matcher(expected_message)
+
+    def _matches(self, matcher_under_test: Matcher[Any]) -> bool:
+        actual = StringDescription()
+        matched = matcher_under_test.matches(self.value_to_match)
+        if matched:
+            matcher_under_test.describe_match(self.value_to_match, actual)
+        return matched and self.expected_message.matches(actual.out)
+
+    def describe_to(self, description: Description) -> None:
+        description.append_text("a matcher which matches the value ").append_description_of(
+            self.value_to_match
+        ).append_text("\ngiving message ").append_description_of(self.expected_message)
+
+    def describe_mismatch(self, matcher_under_test: Matcher[Any], description: Description) -> None:
+        actual_message = StringDescription()
+        if not matcher_under_test.matches(self.value_to_match):
+            description.append_text("mismatched")
+            return
+        matcher_under_test.describe_match(self.value_to_match, actual_message)
+        description.append_text("got message ").append_description_of(actual_message)
+        self.append_diff(actual_message, description)
+
+    def append_diff(self, actual_message, description):
+        if isinstance(self.expected_message, IsEqual) and isinstance(
+            self.expected_message.object, str
+        ):
+            differ = difflib.Differ()
+            diff = differ.compare([self.expected_message.object], [actual_message.out])
+            description.append_text("\ndiff:\n").append_text("\n".join(diff))
+
+
+def matches_with(value_to_match: Any, expected_message: Union[str, Matcher[str]]) -> MatchesWith:
     """TODO"""
-    return not_(mismatches(value_to_match))
+    return MatchesWith(value_to_match, expected_message)
+
+
+def matches(value_to_match: Any) -> MatchesWith:
+    """TODO"""
+    return MatchesWith(value_to_match, anything())
--- tests/unit/matchers/test_matcher.py
+++ tests/unit/matchers/test_matcher.py
@@ -1,7 +1,7 @@
 # encoding=utf-8
 from hamcrest import assert_that, contains_string, has_string
 
-from brunns.matchers.matcher import matches, mismatches, mismatches_with
+from brunns.matchers.matcher import matches, matches_with, mismatches, mismatches_with
 
 
 def test_matcher_mismatches_with():
@@ -11,7 +11,25 @@ def test_matcher_mismatches_with():
     # When
 
     # Then
-    assert_that(banana_matcher, matches("Banana"))
     assert_that(banana_matcher, mismatches("Apple"))
     assert_that(banana_matcher, mismatches_with("Apple", "was 'Apple'"))
-    assert_that(banana_matcher, has_string("a string containing 'Banana'"))
+    assert_that(
+        mismatches_with("Apple", "was 'Apple'"),
+        has_string("a matcher which mismatches the value 'Apple'\ngiving message \"was 'Apple'\""),
+    )
+
+
+def test_matcher_matches_with():
+    # Given
+    banana_matcher = contains_string("Banana")
+
+    # When
+
+    # Then
+
+    assert_that(banana_matcher, matches("Banana"))
+    assert_that(banana_matcher, matches_with("Banana", "was 'Banana'"))
+    assert_that(
+        matches_with("Apple", "was 'Apple'"),
+        has_string("a matcher which matches the value 'Apple'\ngiving message \"was 'Apple'\""),
+    )
